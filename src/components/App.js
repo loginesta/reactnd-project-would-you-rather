1b65e7ceb07a8ba809ed2044c5422475ef400a96
Marc Ginesta <mginesta@adobe.com>
2019-03-26 20:39:55 +0100

    Fix app malfunctioning

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return <div className="App">My App</div>;
   }
 }
 
-export default App;
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
