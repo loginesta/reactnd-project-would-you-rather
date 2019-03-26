2224fa8bea437d0181b5c3d85baaf4683a1d6e0e
Marc Ginesta <mginesta@adobe.com>
2019-03-26 23:57:20 +0100

    Clean after uninstall reactstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,18 +6,6 @@ class Login extends Component {
     return (
       <div>
         <h3>Login Page</h3>
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Dropdown Button
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        ;
       </div>
     );
   }
