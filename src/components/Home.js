34b24c122ebc66b1fab046f52eeda0ba2de5f150
Marc Ginesta <mginesta@adobe.com>
2019-03-29 00:01:19 +0100

    Add logout system

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { unsetAuthedUser } from "../actions/authedUser";
 
 class Home extends Component {
+  handleLogout = () => {
+    const { dispatch } = this.props;
+    dispatch(unsetAuthedUser());
+  };
+
   render() {
     const { avatarURL, id } = this.props.user;
     console.log(this.props);
@@ -9,6 +15,7 @@ class Home extends Component {
       <div>
         <h3>Hello {id}</h3>
         <img src={avatarURL} alt="" />
+        <p onClick={this.handleLogout}>Logout</p>
       </div>
     );
   }
