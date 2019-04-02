89c72c5eefc22992f59ec5b51c01a101274cc770
Marc Ginesta <mginesta@adobe.com>
2019-04-02 23:53:56 +0200

    feat: Sort questions from the list

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -5,11 +5,18 @@ import Question from "./Question";
 class QuestionList extends Component {
   render() {
     const { questions } = this.props;
+    const questionsArray = Object.keys(questions).map(qid => questions[qid]);
+    const sortedQuestions = questionsArray.sort(
+      (a, b) => b.timestamp - a.timestamp,
+    );
+
+    console.log(sortedQuestions);
+
     return (
       <ul>
-        {Object.keys(questions).map(qid => (
+        {Object.keys(sortedQuestions).map(qid => (
           <li key={qid} id={qid}>
-            <Question question={questions[qid]} />
+            <Question question={sortedQuestions[qid]} />
           </li>
         ))}
       </ul>
