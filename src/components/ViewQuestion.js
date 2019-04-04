2f13093a4304c7c9ccc4d16b8fdb5a83395cfcb5
Marc Ginesta <mginesta@adobe.com>
2019-04-04 23:45:18 +0200

    refactor: Extract methods - WIP

--- src/components/ViewQuestion.js
+++ src/components/ViewQuestion.js
@@ -1,15 +1,73 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import authedUser from "../reducers/authedUser";
 
 class ViewQuestion extends Component {
-  render() {
-    const { authedUser, questions, users } = this.props;
+  getQuestionById = () => {
     const questionId = this.props.match.params.question_id;
+    const { questions } = this.props;
     const thisQuestion = Object.values(questions).filter(
       q => q.id === questionId,
     );
-    const question = thisQuestion.shift();
-    console.log(question);
+    return thisQuestion.shift();
+  };
+
+  userAlreadyAnsweredThisQuestion = (authedUserId, question) => {
+    return (
+      question.optionOne.votes.includes(authedUserId) ||
+      question.optionTwo.votes.includes(authedUserId)
+    );
+  };
+
+  showResults = (question, authedUserId) => {
+    console.log(question.optionOne);
+    console.log(question.optionTwo);
+
+    const votesOptionOne = question.optionOne.votes.length,
+      votesOptionTwo = question.optionTwo.votes.length,
+      votesTotal = votesOptionOne + votesOptionTwo,
+      percentageOptionOne = (votesOptionOne / votesTotal) * 100,
+      percentageOptionTwo = (votesOptionTwo / votesTotal) * 100;
+
+    const optionChosenByUser = this.getOptionChosenByUser(
+      question,
+      authedUserId,
+    );
+
+    console.log(percentageOptionOne, percentageOptionTwo);
+    console.log(optionChosenByUser);
+
+    return (
+      <div className="row">
+        <button className="">{question.optionOne.text}</button>
+        <button className="">{question.optionTwo.text}</button>
+      </div>
+    );
+  };
+
+  getOptionChosenByUser = (question, authedUserId) => {
+    if (question.optionOne.votes.includes(authedUserId)) {
+      return "one";
+    } else return "two";
+  };
+
+  showOptions = question => {
+    return (
+      <div className="row">
+        <button>{question.optionOne.text}</button>
+        <button>{question.optionTwo.text}</button>
+      </div>
+    );
+  };
+
+  render() {
+    const { authedUser, users } = this.props;
+    const question = this.getQuestionById();
+    const userAlreadyAnsweredThisQuestion = this.userAlreadyAnsweredThisQuestion(
+      authedUser.id,
+      question,
+    );
+
     return (
       <div className="card">
         <div className="question">
@@ -32,10 +90,9 @@ class ViewQuestion extends Component {
                 <p>
                   <strong>Would you rather:</strong>
                 </p>
-                <ul>
-                  <li>{question.optionOne.text}</li>
-                  <li>{question.optionTwo.text}</li>
-                </ul>
+                {!userAlreadyAnsweredThisQuestion && this.showOptions(question)}
+                {userAlreadyAnsweredThisQuestion &&
+                  this.showResults(question, authedUser.id)}
               </div>
             </div>
           </div>
