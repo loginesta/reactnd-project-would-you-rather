b71be4d6987700b577c68dbfd41d97fbbcbef2d3
Marc Ginesta <mginesta@adobe.com>
2019-04-04 11:05:07 +0200

    refactor: Nav Component improved

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,8 @@ import { connect } from "react-redux";
 import { unsetAuthedUser } from "../actions/authedUser";
 
 class Nav extends Component {
-  handleLogout = () => {
+  handleLogout = e => {
+    e.preventDefault();
     const { dispatch } = this.props;
     dispatch(unsetAuthedUser());
   };
@@ -11,11 +12,63 @@ class Nav extends Component {
   render() {
     const { avatarURL, id } = this.props.user;
     return (
-      <div>
-        <h3>Hello {id}</h3>
-        <img src={avatarURL} alt="" />
-        <p onClick={this.handleLogout}>Logout</p>
-      </div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#defaultNavbar"
+          aria-controls="defaultNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="defaultNavbar">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                New Question
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Leader Board
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="logged-in-dropdown"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <img className="avatar" src={avatarURL} alt="" /> {id}
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="logged-in-dropdown"
+              >
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={this.handleLogout}
+                >
+                  Logout
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
