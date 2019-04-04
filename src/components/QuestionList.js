f2e71c916c3b8ce5aa730b72b76d8ef9210c1dc3
Marc Ginesta <mginesta@adobe.com>
2019-04-04 12:15:06 +0200

    feat: Filtered Questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -8,15 +8,46 @@ class QuestionList extends Component {
     return questionsArray.sort((a, b) => b.timestamp - a.timestamp);
   };
 
-  render() {
+  filterQuestions = criteria => {
     const sortedQuestions = this.sortQuestions(this.props.questions);
+    const { authedUser } = this.props;
+    let questions = [];
+
+    if (criteria === "answered") {
+      questions = sortedQuestions.filter(function(question) {
+        return (
+          question.optionOne.votes.includes(authedUser.id) ||
+          question.optionTwo.votes.includes(authedUser.id)
+        );
+      });
+    } else {
+      questions = sortedQuestions.filter(function(question) {
+        return (
+          question.optionOne.votes.indexOf(authedUser.id) === -1 &&
+          question.optionTwo.votes.indexOf(authedUser.id) === -1
+        );
+      });
+    }
+    return questions;
+  };
+
+  render() {
+    const filteredQuestions = this.filterQuestions("answered");
 
     return (
       <div className="distance-from-navbar">
+        <div className="btn-group" role="group">
+          <button type="button" className="btn btn-outline-secondary">
+            Answered
+          </button>
+          <button type="button" className="btn btn-outline-secondary">
+            Unanswered
+          </button>
+        </div>
         <ul>
-          {Object.keys(sortedQuestions).map(qid => (
+          {Object.keys(filteredQuestions).map(qid => (
             <li key={qid} id={qid}>
-              <Question question={sortedQuestions[qid]} />
+              <Question question={filteredQuestions[qid]} />
             </li>
           ))}
         </ul>
@@ -25,8 +56,9 @@ class QuestionList extends Component {
   }
 }
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ authedUser, questions }) {
   return {
+    authedUser,
     questions,
   };
 }
