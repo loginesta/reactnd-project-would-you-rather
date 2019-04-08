baed33867d9bf25ecbf6ab3e849aed58e7ed54c5
Marc Ginesta <mginesta@adobe.com>
2019-04-08 09:05:42 +0200

    refactor: NotFound and Question as stateless functional components

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NotFound extends Component {
-  render() {
-    return <h1>404 / Page Not Found</h1>;
-  }
-}
+const NotFound = () => {
+  return <h1>404 / Page Not Found</h1>;
+};
 
 export default NotFound;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,51 +1,49 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-class Question extends Component {
-  render() {
-    const { question, users } = this.props;
-    return (
-      <div className="card">
-        <div className="question">
-          <div className="card-header">
-            <div className="row align-items-center">
-              <div className="col-auto mr-auto">
-                <img
-                  className="avatar"
-                  src={users[question.author].avatarURL}
-                  alt=""
-                />
-                {users[question.author].name} asks...
-              </div>
-              <div className="col-auto">
-                <Link
-                  to={`/questions/${question.id}`}
-                  className="btn btn-outline-primary"
-                >
-                  View poll
-                </Link>
-              </div>
+const Question = props => {
+  const { question, users } = props;
+  return (
+    <div className="card">
+      <div className="question">
+        <div className="card-header">
+          <div className="row align-items-center">
+            <div className="col-auto mr-auto">
+              <img
+                className="avatar"
+                src={users[question.author].avatarURL}
+                alt=""
+              />
+              {users[question.author].name} asks...
+            </div>
+            <div className="col-auto">
+              <Link
+                to={`/questions/${question.id}`}
+                className="btn btn-outline-primary"
+              >
+                View poll
+              </Link>
             </div>
           </div>
-          <div className="question-body">
-            <div className="question-options">
-              <div className="col-sm">
-                <p>
-                  <strong>Would you rather:</strong>
-                </p>
-                <ul>
-                  <li>{question.optionOne.text}</li>
-                  <li>{question.optionTwo.text}</li>
-                </ul>
-              </div>
+        </div>
+        <div className="question-body">
+          <div className="question-options">
+            <div className="col-sm">
+              <p>
+                <strong>Would you rather:</strong>
+              </p>
+              <ul>
+                <li>{question.optionOne.text}</li>
+                <li>{question.optionTwo.text}</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 function mapStateToProps({ users }) {
   return {
